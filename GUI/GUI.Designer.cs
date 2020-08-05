2a71fd402c81cb16197856ab9b0c4e5ae5accca8
Cesito <celso.almeida.leite@hotmail.com>
2020-08-05 13:08:33 -0300

    Inclusão da barra de status no Layout

--- GUI/GUI.Designer.cs
+++ GUI/GUI.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             this.UHeader = new System.Windows.Forms.Panel();
-            this.UMenu = new System.Windows.Forms.Panel();
-            this.USection = new System.Windows.Forms.Panel();
             this.linhaHeader = new System.Windows.Forms.Panel();
+            this.UStaus = new System.Windows.Forms.Panel();
+            this.UMenu = new System.Windows.Forms.Panel();
             this.linhaMenu = new System.Windows.Forms.Panel();
+            this.USection = new System.Windows.Forms.Panel();
+            this.linhaStatus = new System.Windows.Forms.Panel();
             this.UHeader.SuspendLayout();
+            this.UStaus.SuspendLayout();
             this.UMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,25 +50,6 @@
             this.UHeader.Size = new System.Drawing.Size(893, 64);
             this.UHeader.TabIndex = 0;
             // 
-            // UMenu
-            // 
-            this.UMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
-            this.UMenu.Controls.Add(this.linhaMenu);
-            this.UMenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.UMenu.Location = new System.Drawing.Point(0, 64);
-            this.UMenu.Name = "UMenu";
-            this.UMenu.Size = new System.Drawing.Size(42, 481);
-            this.UMenu.TabIndex = 0;
-            // 
-            // USection
-            // 
-            this.USection.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.USection.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.USection.Location = new System.Drawing.Point(42, 64);
-            this.USection.Name = "USection";
-            this.USection.Size = new System.Drawing.Size(851, 481);
-            this.USection.TabIndex = 1;
-            // 
             // linhaHeader
             // 
             this.linhaHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
@@ -75,14 +59,52 @@
             this.linhaHeader.Size = new System.Drawing.Size(893, 2);
             this.linhaHeader.TabIndex = 0;
             // 
+            // UStaus
+            // 
+            this.UStaus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.UStaus.Controls.Add(this.linhaStatus);
+            this.UStaus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.UStaus.Location = new System.Drawing.Point(0, 513);
+            this.UStaus.Name = "UStaus";
+            this.UStaus.Size = new System.Drawing.Size(893, 32);
+            this.UStaus.TabIndex = 2;
+            // 
+            // UMenu
+            // 
+            this.UMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.UMenu.Controls.Add(this.linhaMenu);
+            this.UMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.UMenu.Location = new System.Drawing.Point(0, 64);
+            this.UMenu.Name = "UMenu";
+            this.UMenu.Size = new System.Drawing.Size(42, 449);
+            this.UMenu.TabIndex = 3;
+            // 
             // linhaMenu
             // 
             this.linhaMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(115)))), ((int)(((byte)(117)))));
             this.linhaMenu.Dock = System.Windows.Forms.DockStyle.Right;
             this.linhaMenu.Location = new System.Drawing.Point(41, 0);
             this.linhaMenu.Name = "linhaMenu";
-            this.linhaMenu.Size = new System.Drawing.Size(1, 481);
+            this.linhaMenu.Size = new System.Drawing.Size(1, 449);
             this.linhaMenu.TabIndex = 0;
+            // 
+            // USection
+            // 
+            this.USection.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.USection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.USection.Location = new System.Drawing.Point(42, 64);
+            this.USection.Name = "USection";
+            this.USection.Size = new System.Drawing.Size(851, 449);
+            this.USection.TabIndex = 4;
+            // 
+            // linhaStatus
+            // 
+            this.linhaStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(115)))), ((int)(((byte)(117)))));
+            this.linhaStatus.Dock = System.Windows.Forms.DockStyle.Top;
+            this.linhaStatus.Location = new System.Drawing.Point(0, 0);
+            this.linhaStatus.Name = "linhaStatus";
+            this.linhaStatus.Size = new System.Drawing.Size(893, 1);
+            this.linhaStatus.TabIndex = 0;
             // 
             // GUI
             // 
@@ -91,10 +113,13 @@
             this.ClientSize = new System.Drawing.Size(893, 545);
             this.Controls.Add(this.USection);
             this.Controls.Add(this.UMenu);
+            this.Controls.Add(this.UStaus);
             this.Controls.Add(this.UHeader);
             this.Name = "GUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GUI";
             this.UHeader.ResumeLayout(false);
+            this.UStaus.ResumeLayout(false);
             this.UMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -103,9 +128,11 @@
         #endregion
 
         private System.Windows.Forms.Panel UHeader;
-        private System.Windows.Forms.Panel UMenu;
-        private System.Windows.Forms.Panel USection;
         private System.Windows.Forms.Panel linhaHeader;
+        private System.Windows.Forms.Panel UStaus;
+        private System.Windows.Forms.Panel linhaStatus;
+        private System.Windows.Forms.Panel UMenu;
         private System.Windows.Forms.Panel linhaMenu;
+        private System.Windows.Forms.Panel USection;
     }
 }
